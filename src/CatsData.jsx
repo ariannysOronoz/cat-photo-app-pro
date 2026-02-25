5ec46e91f4f36f05feaa432ce53b03e1ea73c924
ariannysOronoz <ariom0910@gmail.com>
2026-02-25 11:48:23 -0300

    feat: add catsData array

---
+++ src/CatsData.jsx
@@ -0,0 +1,20 @@
+export const catsData = [
+  {
+    id: 1,
+    title: "Relaxing Cat",
+    url: "https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg",
+    description: "Un tierno gato naranja relajándose al sol."
+  },
+  {
+    id: 2,
+    title: "Lasagna Lover",
+    url: "https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg",
+    description: "Porque todos sabemos que los gatos aman la lasaña."
+  },
+  {
+    id: 3,
+    title: "The Gang",
+    url: "https://cdn.freecodecamp.org/curriculum/cat-photo-app/cats.jpg",
+    description: "Un grupo de amigos explorando el mundo exterior."
+  }
+];

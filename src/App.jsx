be3f2469e81d14105c18d6898d3aa14d1ab62603
ariannysOronoz <ariom0910@gmail.com>
2026-02-25 12:07:37 -0300

    feat: add CatCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { Box } from '@mui/material';
+import { Box , Container, Grid} from '@mui/material';
 import Hero from './components/Hero'; 
 import Navbar from './components/Navbar';
-
+import CatCard from './components/CatCard';
+import { catsData } from './catsData';
 function App() {
   return (
     <Box>
       <Navbar />
       <Hero />
+
+      <Container maxWidth="lg" sx={{ mt: 8 }}>
+
+      <Grid container spacing={4}>
+          {catsData.map((cat) => (
+            <Grid item key={cat.id} xs={12} sm={6} md={4}>
+              <CatCard 
+                url={cat.url} 
+                title={cat.title} 
+                description={cat.description} 
+              />
+            </Grid>
+          ))}
+        </Grid>
+
+      </Container>
     </Box>
   );
 }
---
+++ src/components/CatCard.jsx
@@ -0,0 +1,31 @@
+import { Card, CardMedia, CardContent, Typography, CardActionArea } from '@mui/material';
+
+export default function CatCard({ url, title, description }) {
+  return (
+    <Card sx={{ 
+      height: '100%', 
+      display: 'flex', 
+      flexDirection: 'column',
+      borderRadius: 4,
+      transition: 'transform 0.2s',
+      '&:hover': { transform: 'scale(1.03)' } // Efecto de zoom al pasar el mouse
+    }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="220"
+          image={url}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div" fontWeight="bold">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
